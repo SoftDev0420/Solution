f75f58ccfd7fe68ebc3472e8e97ad9f1fe183a81
SoftDev0420 <softdev0420@gmail.com>
2016-10-24 10:01:11 +0200

    Add helpers

---
+++ eword/Helpers/DataClass.swift
@@ -0,0 +1,13 @@
+//
+//  DataClass.swift
+//  eword
+//
+//  Created by Admin on 24/10/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+class DataClass {
+    static let instance = DataClass()
+    
+    var AppId: String?
+}
